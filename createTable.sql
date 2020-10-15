de937e8b45e044a0a334abe88bba7754eb682cbd
C. Henry White <37254359+HoneyBeebus@users.noreply.github.com>
2020-10-15 14:53:11 -0700

    updated schema

--- createTable.sql
+++ createTable.sql
@@ -1,15 +1,28 @@
 CREATE TABLE Analyses (
     AID INTEGER PRIMARY KEY,
     UID INTEGER,
-	client VARCHAR(15),
-	run_Date DATE,
-	risk_Inherent CHAR(2),
-	risk_Residual CHAR(2),
-	notes VARCHAR(280)
+	lossScenario VARCHAR(15),
+	runDate DATE,
+	overallResidualRisk CHAR(2),
+	potentialLossMagnitude INTEGER,
+	resistanceStrengthVulnerabilityInherent INTEGER,
+	resistanceStrengthVulnerabilityControls INTEGER,
+	probabiltyOfActionDeterrenceInherent INTEGER,
+	probabiltyOfActionDeterrenceControls INTEGER,
+	contactFrequencyAvoidanceInherent INTEGER,
+	contactFrequencyAvoidanceControls INTEGER,
+	threatCapabilitiy INTEGER,
+	secondaryLossProbability INTEGER,
+	primaryLossMagnitudeInherent INTEGER,
+	primaryLossMagnitudeControls INTEGER,
+	secondaryLossMagnitudeResponsiveInherent INTEGER,
+	secondaryLossMagnitudeResponsiveControls INTEGER,
+	notes VARCHAR(1000),
 );
 
 CREATE TABLE Users (
 	UID INTEGER PRIMARY KEY,
 	username VARCHAR(20),
+	password VARCHAR(20),
 	role CHAR(1)
 );
--- insert.sql
+++ insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO Analyses VALUES(1, 1, "R. Kelly", "2020-04-20", "VL", "H", "wolfeschlegelsteinhausenbergerdorff");
-INSERT INTO Analyses VALUES(2, 1, "Jimmy Neutron", "2020-06-09", "H", "M", "Goddard");
+INSERT INTO Analyses VALUES(1, 1, "R. Kelly", "2020-04-20", "VL", 100000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "wolfeschlegelsteinhausenbergerdorff");
+INSERT INTO Analyses VALUES(2, 1, "Jimmy Neutron", "2020-06-09", "H", 100000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "Goddard");
 
-INSERT INTO Users VALUES(1, "J. Smith", "A");
-INSERT INTO Users VALUES(2, "B. Johnson", "E");
+INSERT INTO Users VALUES(1, "J. Smith", "Ahbgasdf", "A");
+INSERT INTO Users VALUES(2, "B. Johnson", "Fucucuacuu", "E");
