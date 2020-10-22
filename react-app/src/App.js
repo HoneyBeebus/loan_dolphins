165be60da81229f69285004a662db0ced4913602
DieGo <diegog367@gmail.com>
2020-10-21 17:45:19 -0700

    Renamed react app folder

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          asdkfjasdkfjn
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
