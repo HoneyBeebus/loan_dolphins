310393228a232fffa2e64c3ceb28370d5e6bd641
C. Henry White <37254359+HoneyBeebus@users.noreply.github.com>
2020-10-26 12:10:53 -0700

    Tried to implement data grid. Errors :(

--- react-app/src/Routes.js
+++ react-app/src/Routes.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 
 // Pages
 import LoginPage from './pages/LoginPage';
 import ExamplePage from './pages/ExamplePage';
 import OutcomePage from './pages/OutcomePage';
+import history from './history';
+
 
 function Routes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={LoginPage}/>
 			<Route path="/test" exact component={ExamplePage}/>
-			<Route path ="/test" exact component={OutcomePage}/>
+			<Route path ="/outcome" exact component={OutcomePage}/>
 		</Switch>
 	);
 }
--- react-app/src/pages/OutcomePage.js
+++ react-app/src/pages/OutcomePage.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import ReactDataGrid from 'react-data-grid';
 import { Button } from '@material-ui/core';
 
-function OutcomePage() {
-	return (<>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	</>);
-}
-
 export default OutcomePage;
+
+//export default function DataGrid() {
+  
+  const columns = [
+    { key: "lossScenario", name: "Loss Scenario" },
+    { key: "date", name: "Date" },
+    { key: "overallResidualRisk", name: "Overall Residual Risk" },
+    { key: "potentialLossMagnitude", name: "Potential Loss Magnitude"}
+  ];
+
+  const rows = [
+    { lossScenatio: "Ada Lovelace", date: "04-21-2019", overallResidualRisk: "Very High", potentialLossMagnitude: "4" },
+  ];
+
+function OutcomePage() {
+		return (<div>
+			<h1>Outcome</h1>
+      // <ReactDataGrid
+       // columns={columns}
+        //rowGetter={i => rows[i]}
+        //rowsCount={3}
+      />
+      <Button variant="contained" color="primary">Cancel</Button>
+      <Button variant="contained" color="primary">Save</Button>
+      <img src="tree.png" className="App-header" alt="logo" />
+
+    </div>);
+}
