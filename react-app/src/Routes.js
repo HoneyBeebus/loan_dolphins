e30f9b8041d3ee9eff626febbe1a2997e14c9da4
C. Henry White <37254359+HoneyBeebus@users.noreply.github.com>
2020-10-26 10:15:28 -0700

    created outcomepage.js and added to routes

--- react-app/src/Routes.js
+++ react-app/src/Routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 // Pages
 import LoginPage from './pages/LoginPage';
 import ExamplePage from './pages/ExamplePage';
+import OutcomePage from './pages/OutcomePage';
 
 function Routes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={LoginPage}/>
 			<Route path="/test" exact component={ExamplePage}/>
+			<Route path ="/test" exact component={OutcomePage}/>
 		</Switch>
 	);
 }
---
+++ react-app/src/pages/OutcomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function OutcomePage() {
+	return (<>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+	</>);
+}
+
+export default OutcomePage;
