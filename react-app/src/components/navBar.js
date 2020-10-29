7df17fc71d14d9e30607589f13d5d122672b5a6f
C. Henry White <37254359+HoneyBeebus@users.noreply.github.com>
2020-10-28 18:18:17 -0700

    Created navBar.js

---
+++ react-app/src/components/navBar.js
@@ -0,0 +1,32 @@
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+{/*import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+export default function SimpleBreadcrumbs() {
+  return (
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link color="inherit" href="./dashboard" onClick={handleClick}>
+        Dashboard
+      </Link>
+      <Link color="inherit" href="./outcomes" onClick={handleClick}>
+        Outcomes
+      </Link>
+      <Typography color="textPrimary">Breadcrumb</Typography>
+    </Breadcrumbs>
+  );
+} */}
+
+export default function navBar() {
+    
+}
--- react-app/src/pages/LoginPage.js
+++ react-app/src/pages/LoginPage.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
+import IconBreadcrumbs from '../components/navBar';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 class LoginPage extends React.Component {
 	render() {
 		return (<div>
+			<SideNav
+    			onSelect={(selected) => {
+        				// Add your code here
+    			}}
+			>
+    			<SideNav.Toggle />
+    			<SideNav.Nav defaultSelected="home">
+        			<NavItem eventKey="home">
+            			<NavIcon>
+                			<i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            			</NavIcon>
+            			<NavText>
+                			Home
+            			</NavText>
+        			</NavItem>
+        			<NavItem eventKey="charts">
+            			<NavIcon>
+                			<i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            			</NavIcon>
+            			<NavText>
+                			Charts
+            			</NavText>
+            		<NavItem eventKey="charts/linechart">
+                		<NavText>
+                    		Line Chart
+                		</NavText>
+            		</NavItem>
+            		<NavItem eventKey="charts/barchart">
+                		<NavText>
+                    		Bar Chart
+                		</NavText>
+            		</NavItem>
+        		</NavItem>
+    		</SideNav.Nav>
+		</SideNav>
+  			<br />
 			<h1>Login</h1>
 			<TextField label="username"/>
 			<TextField type="password" label="password"/>
