761307250bf6e6e75ac9cb42b9120eb175a0d2cb
C. Henry White <37254359+HoneyBeebus@users.noreply.github.com>
2020-12-02 16:41:53 -0800

    changed table names for outcome

--- react-app/src/components/DataTree.jsx
+++ react-app/src/components/DataTree.jsx
@@ -21,13 +21,13 @@ export default function FullWidthGrid() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+          <Paper className={classes.paper}>Overall Risk<br></br>Inherent:<br></br>Residual:</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper className={classes.paper}>Primary Risk<br></br>Inherent:<br></br>Residual:</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper className={classes.paper}>Secondary Risk<br></br>Inherent:<br></br>Residual:</Paper>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Paper className={classes.paper}>xs=6 sm=3</Paper>
