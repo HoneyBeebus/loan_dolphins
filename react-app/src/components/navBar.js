a5f5dabadb3e0057bd88a5faac94a231122802ae
DieGo <diegog367@gmail.com>
2020-12-02 20:26:17 -0800

    Reordered navbar

--- react-app/src/components/navBar.js
+++ react-app/src/components/navBar.js
@@ -16,15 +16,13 @@ export class Navigation extends Component {
       <Navbar bg="dark" expand="lg">
         <img src="oportun_logo_circle.png" width="45" height="20"alt="Logo" />
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
-          <NavLink className="d-inline p-2 bg-dark text-white" to="/" onClick={this.logout}>Logout</NavLink>
+        <Nav id="basic-navbar-nav">
           <NavLink className="d-inline p-2 bg-dark text-white" to="/dashboard">Dashboard</NavLink>
-          <NavLink className="d-inline p-2 bg-dark text-white" to="/outcome">Outcomes</NavLink>
-          <NavLink className="d-inline p-2 bg-dark text-white" to="/matrix">Matricies</NavLink>
           <NavLink className="d-inline p-2 bg-dark text-white" to="/risk">Risk Analysis</NavLink>
-          
+          <NavLink className="d-inline p-2 bg-dark text-white" to="/matrix">Matricies</NavLink>
         </Nav>
+        <Navbar.Collapse className="justify-content-end">
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/" onClick={this.logout}>Logout</NavLink>
         </Navbar.Collapse>
       </Navbar>
     )
